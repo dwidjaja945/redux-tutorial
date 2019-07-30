5fc18fda13aff0ec93c58da06dead4adec9d7ef2
Dylan Widjaja <dwidjaja945@gmail.com>
2019-07-29 19:12:38 -0700

    [add] added search component

---
+++ client/src/components/Search/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Search.css';
+
+function Search(props) {
+    return (
+        <div className='search'>
+            <div className='search_container'>
+                Search Container:
+            </div>
+            <input type="text" />
+        </div>
+    );
+}
+
+export default Search;
